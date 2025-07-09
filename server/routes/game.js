4dddae55e6d9bd186aabd313d2f216dbbb31ff7a
Darbes <74102593+Darbes@users.noreply.github.com>
2025-07-09 23:45:57 +0530

    Update game.js

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,9 +1,3 @@
-const GameSchema = new mongoose.Schema({
-  players: [String],
-  completed: Boolean,
-  winner: String
-}, { timestamps: true }); // <-- add this
-
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
